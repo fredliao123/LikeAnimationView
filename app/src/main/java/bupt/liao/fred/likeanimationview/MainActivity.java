bf11c7d9c504b3bd6b3339d8a0b852e035efb61f
fredliao <724430854@qq.com>
2017-11-18 12:21:56 +1100

    Whole Project

--- app/src/main/java/bupt/liao/fred/likeanimationview/MainActivity.java
+++ app/src/main/java/bupt/liao/fred/likeanimationview/MainActivity.java
@@ -2,12 +2,51 @@ package bupt.liao.fred.likeanimationview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import bupt.liao.fred.likeanimationview.view.LikeUpView;
+import bupt.liao.fred.likeanimationview.view.LikeView;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText edNum;
+    LikeUpView likeUpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edNum = (EditText) findViewById(R.id.ed_num);
+        likeUpView = (LikeUpView) findViewById(R.id.newLikeUpView);
+
+        likeUpView.setLikeUpClickListener(new LikeView.LikeUpClickListener() {
+
+            @Override
+            public void LikeUpFinish() {
+
+            }
+
+            @Override
+            public void LikeDownFinish() {
+
+            }
+        });
+    }
+
+    public void setNum(View v) {
+        try {
+            int num = Integer.valueOf(edNum.getText().toString().trim());
+            likeUpView.setCount(num).setLikeUp(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Integer Only", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
